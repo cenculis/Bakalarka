d1e1dc7eaf4f9f47222446c841a21e0ad99a379d
cenculis <xtibensky@stuba.sk>
2022-03-04 21:28:19 +0100

    small changes in code

--- zatial funkcne api a lib/src/AeroShield.cpp
+++ zatial funkcne api a lib/src/AeroShield.cpp
@@ -11,7 +11,7 @@
   Attribution-NonCommercial 4.0 International License.
 
   Created by .... 
-  Last update: 05.02.2022.
+  Last update: 02.03.2022.
 */
 
 
@@ -20,10 +20,7 @@
 // Initializes adreeses needed for communication with as5600
 AeroShield::AeroShield()
 {
-  _ams5600_Address = 0x36;
-  _raw_ang_hi = 0x0c;
-  _raw_ang_lo = 0x0d;
-  _stat = 0x0b;
+ 
 }
 
 
@@ -39,7 +36,8 @@ float AeroShield::begin(bool isDetected){                             // Board i
   #elif ARDUINO_ARCH_SAMD                                             // For SAMD architecture boards
     Wire.begin();                                                     // Use Wire object
   #endif
-
+  
+  
         if(isDetected == 0 ){                                           // If magnet not detected go on
     while(1){                                                           // Go forever until magnet detected 
         if(isDetected == 1 ){                                           // If magnet detected
@@ -50,19 +48,18 @@ float AeroShield::begin(bool isDetected){                             // Board i
             AutomationShield.serialPrint("Can not detect magnet \n");   // Print information then go back to check while statement
       }
     }
-  }                                                                                       
+  }       
 } 
 
 
-float AeroShield::convertRawAngleToDegrees(word newAngle) {        // Function for converting raw angle(0-4096) to degrees(0-360°) 
+float AeroShield::convertRawAngleToDegrees(word newAngle) {             // Function for converting raw angle(0-4096) to degrees(0-360°) 
   float retVal = newAngle * 0.087;                                      // 360°/4096=0.087° times the raw value
   ang = retVal;                               
   return ang;                                                           // Return angle value in degrees 
 }
 
 
-float AeroShield::calibration(word RawAngle) {                     // Calibration 
-
+float AeroShield::calibration(word RawAngle) {                          // Calibration 
   AutomationShield.serialPrint("Calibration running...\n");             // Print info 
   startangle=0;                                                         // Zero out Variable(precaution)
   analogWrite(AERO_UPIN,50);                                            // Power the actuator, swing the pendulum 
@@ -80,23 +77,23 @@ float AeroShield::calibration(word RawAngle) {                     // Calibratio
       }
 
   AutomationShield.serialPrint("Calibration done");
-    return startangle;
+    return startangle;                                                  // Return start angle
 }
 
 
-  float AeroShield::referenceRead(void) {                                             // Reference read
+  float AeroShield::referenceRead(void) {                                                  // Reference read
   referenceValue = (float)analogRead(AERO_RPIN);                                           // Reads the actual analog value of potentiometer runner
   referencePercent = AutomationShield.mapFloat(referenceValue, 0.0, 1024.0, 0.0, 100.0);   // Remapps the analog value from original range 0.0-1023 to percentual range 0.0-100.0
   return referencePercent;                                                                 // Returns the percentual position of potentiometer runner
 }
 
-void AeroShield::actuatorWrite(float PotPercent) {                                    // Actuator write
+void AeroShield::actuatorWrite(float PotPercent) {                                         // Actuator write
   float mappedValue = AutomationShield.mapFloat(PotPercent, 0.0, 100.0, 0.0, 255.0);       // Takes the float type percentual value 0.0-100.0 and remapps it to range 0.0-255.0
   mappedValue = AutomationShield.constrainFloat(mappedValue, 0.0, 255.0);                  // Constrains the remapped value to fit the range 0.0-255.0 - safety precaution
   analogWrite(AERO_UPIN, (int)mappedValue);                                                // Write remapped value to actuator pin 
 }
 
-float AeroShield::currentMeasure(void){                                               // Measuring current drawn by DC motor 
+float AeroShield::currentMeasure(void){                                                    // Measuring current drawn by DC motor 
   for(int i=0 ; i<repeatTimes ; i++){                                                      // Function for callculating mean current value 
      voltageValue= analogRead(VOLTAGE_SENSOR_PIN);                                         // Read a value from the INA169 
      voltageValue= (voltageValue * voltageReference) / 1024;                               // Remap the ADC value into a voltage number (5V reference)
@@ -135,65 +132,64 @@ int AeroShield::detectMagnet()                                                  
 }
 
 
-int AeroShield::getMagnetStrength()                                                  // Function for getting the strength of magnet 
+int AeroShield::getMagnetStrength()                 // Function for getting the strength of magnet 
 {
-  int magStatus;                                                                     // Auxiliary variable
-  int retVal = 0;                                                                    // Auxiliary variable
-  magStatus = readOneByte(_stat);                                                    // Another function for communication with senzor, called from this library     
+  int magStatus;                                    // Auxiliary variable
+  int retVal = 0;                                   // Auxiliary variable
+  magStatus = readOneByte(_stat);                   // Another function for communication with senzor, called from this library     
 
-  if (detectMagnet() == 1)                                                           // Return 0 if no magnet is detected
+  if (detectMagnet() == 1)                          // Return 0 if no magnet is detected
   {
-    retVal = 2;                                                                      // Return 2 if magnet is just right
+    retVal = 2;                                     // Return 2 if magnet is just right
     if (magStatus & 0x10)
-      retVal = 1;                                                                    // Return 1 if magnet is too weak
+      retVal = 1;                                   // Return 1 if magnet is too weak
     else if (magStatus & 0x08)
-      retVal = 3;                                                                    // Return 3 if magnet is too strong
+      retVal = 3;                                   // Return 3 if magnet is too strong
   }
 
-  return retVal;                                                                     // Return value 
+  return retVal;                                    // Return value 
 }
 
 
-int AeroShield::readOneByte(int in_adr)             // Function for communicating with the senzor using 1 bite 
+int AeroShield::readOneByte(int in_adr)             // Function for communicating with the senzor using 1 Byte 
 {
   int retVal = -1;
   Wire.beginTransmission(_ams5600_Address);         // Initialise wire transmission 
-  Wire.write(in_adr);                               // Write 1bite 
+  Wire.write(in_adr);                               // Write 4 bits 
   Wire.endTransmission();                           // End wire transmission 
   Wire.requestFrom(_ams5600_Address, 1);            // Request answer  
-  while (Wire.available() == 0)                     // Wait for returning bite 
-    ;
-  retVal = Wire.read();                             // Store returning bite 
+  while (Wire.available() == 0);                    // Wait for returning bits 
 
-  return retVal;                                    // Return stored bite 
+  retVal = Wire.read();                             // Store returning bits 
+
+  return retVal;                                    // Return stored bits 
 }
 
 
-word AeroShield::readTwoBytes(int in_adr_hi, int in_adr_lo)          // Function for communicating with the senzor using 2 bites 
+word AeroShield::readTwoBytes(int in_adr_hi, int in_adr_lo)          // Function for communicating with the senzor using 2 Bytes 
 {
   word retVal = -1;
 
   /* Read Low Byte */
   Wire.beginTransmission(_ams5600_Address);        // Initialise wire transmission 
-  Wire.write(in_adr_lo);                           // Write 1bite 
+  Wire.write(in_adr_lo);                           // Write 4 bits 
   Wire.endTransmission();                          // End wire transmission 
   Wire.requestFrom(_ams5600_Address, 1);           // Request answer  
-  while (Wire.available() == 0)                    // Wait for returning bite 
-    ;
-  int low = Wire.read();                           // Store first returning bite 
+  while (Wire.available() == 0);                    // Wait for returning bits 
+
+  int low = Wire.read();                           // Store first returning bits 
 
   /* Read High Byte */
   Wire.beginTransmission(_ams5600_Address);        // Initialise wire transmission 
-  Wire.write(in_adr_hi);                           // Write 1bite 
+  Wire.write(in_adr_hi);                           // Write 4 bits   
   Wire.endTransmission();                          // End wire transmission 
   Wire.requestFrom(_ams5600_Address, 1);           // Request answer  
-  while (Wire.available() == 0)                    // Wait for returning bite 
-    ;
+  while (Wire.available() == 0);                    // Wait for returning bits 
 
-  word high = Wire.read();                         // Store second returning bite 
+  word high = Wire.read();                         // Store second returning bits 
 
   high = high << 8;                                // bitwise left shift
   retVal = high | low;
 
-  return retVal;                                   // Return stored bites 
+  return retVal;                                   // Return stored bits 
 }
